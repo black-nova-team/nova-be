fdb40b1e4ae11bb1e232cef62a825f5f17f9ec13
Kimcheolhui <cjfgml0306@naver.com>
2025-01-12 02:02:37 +0900

    chore: apply configmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { SlackModule } from './slack/slack.module';
 
 @Module({
-  imports: [SlackModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SlackModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
