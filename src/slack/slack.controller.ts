ef4392eb4d43748e689a611fd31cdaa8792a1004
Kimcheolhui <cjfgml0306@naver.com>
2025-01-11 23:26:42 +0900

    feat: add info post

--- src/slack/slack.controller.ts
+++ src/slack/slack.controller.ts
@@ -1,10 +1,33 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('slack')
 export class SlackController {
   constructor() {}
 
-
   @Post('info')
-  async sendInfo()
+  @UseInterceptors(FileInterceptor('image')) // body에 'image' 필드로 파일을 받음
+  async sendInfo(
+    @Query('username') username: string,
+    @Query('inputText') inputText: string,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    // 요청받은 데이터를 로그로 출력 (예시)
+    console.log('Username:', username);
+    console.log('InputText:', inputText);
+    console.log('Image:', image.originalname);
+
+    return {
+      message: 'Data received successfully',
+      username,
+      inputText,
+      imageName: image.originalname,
+    };
+  }
 }
