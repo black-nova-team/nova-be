9fc4845d2a06ce03534fcc3a157c9187153d55c6
Kimcheolhui <cjfgml0306@naver.com>
2025-01-12 02:02:31 +0900

    feat: apply image module to info post

--- src/slack/slack.controller.ts
+++ src/slack/slack.controller.ts
@@ -6,28 +6,47 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ImageService } from 'src/image/image.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('slack')
 export class SlackController {
-  constructor() {}
+  constructor(
+    private readonly imageService: ImageService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Post('info')
-  @UseInterceptors(FileInterceptor('image')) // body에 'image' 필드로 파일을 받음
+  @UseInterceptors(FileInterceptor('image'))
   async sendInfo(
     @Query('username') username: string,
     @Query('inputText') inputText: string,
     @UploadedFile() image: Express.Multer.File,
   ) {
-    // 요청받은 데이터를 로그로 출력 (예시)
     console.log('Username:', username);
     console.log('InputText:', inputText);
     console.log('Image:', image.originalname);
 
-    return {
-      message: 'Data received successfully',
-      username,
-      inputText,
-      imageName: image.originalname,
-    };
+    // 1. 이미지 변환 API 요청
+    // 우선 생략
+
+    // 2. 반환된 이미지 S3에 업로드
+    const imageS3Key = await this.imageService.uploadImage(image);
+
+    // 3. S3 key와 함께 DB에 저장
+
+    const newRecord = await this.prismaService.slack.create({
+      data: {
+        username,
+        inputText,
+        imageKey: imageS3Key,
+        manittoId: null, // 필요한 경우 null이 아닌 다른 값을 설정
+        correct: false,
+        released: false,
+        createdAd: new Date(),
+      },
+    });
+
+    return newRecord;
   }
 }
--- src/slack/slack.module.ts
+++ src/slack/slack.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SlackController } from './slack.controller';
 import { SlackService } from './slack.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ImageModule } from 'src/image/image.module';
 
 @Module({
+  imports: [PrismaModule, ImageModule, ConfigModule],
   controllers: [SlackController],
   providers: [SlackService],
 })
