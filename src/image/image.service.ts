944e947fd691fd0344e3f004dfb147082d1ca6aa
Kimcheolhui <cjfgml0306@naver.com>
2025-01-12 09:48:40 +0900

    chore: change s3 upload package

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,5 +1,6 @@
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { S3Client } from '@aws-sdk/client-s3';
+import { Upload } from '@aws-sdk/lib-storage';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -16,21 +17,38 @@ export class ImageService {
     console.log(file);
     const key = `${Date.now()}-${Math.random().toString(36).substring(2)}${file.originalname}`;
 
-    const command = new PutObjectCommand({
-      Bucket: this.bucketName,
-      Key: key,
-      Body: file.buffer,
-      Metadata: {
-        originalName: encodeURIComponent(file.originalname),
+    const upload = new Upload({
+      client: this.s3Client,
+      params: {
+        Bucket: this.bucketName,
+        Key: key,
+        Body: file.buffer,
       },
     });
 
     try {
-      await this.s3Client.send(command);
-      return key;
+      const result = await upload.done();
+      return result.Key;
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('Failed to upload file');
+      console.error(error);
+      throw error;
     }
+
+    // const command = new PutObjectCommand({
+    //   Bucket: this.bucketName,
+    //   Key: key,
+    //   Body: file.buffer,
+    //   Metadata: {
+    //     originalName: encodeURIComponent(file.originalname),
+    //   },
+    // });
+
+    // try {
+    //   await this.s3Client.send(command);
+    //   return key;
+    // } catch (error) {
+    //   console.log(error);
+    //   throw new InternalServerErrorException('Failed to upload file');
+    // }
   }
 }
